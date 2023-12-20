81f91c572669fad364c46f53653772dcc2c07a55
jmctheowl8 <john.cabrera@lc.cuny.edu>
2023-12-19 22:32:59 -0500

    add async and await to post route

--- server/server.js
+++ server/server.js
@@ -51,12 +51,22 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 //create
 app.post("/api/v1/restaurants", (req,res)=>{
     console.log(req)
-    res.status(201).json({
-        status: "sucess",
-        data:{
-            restaurant: "mcdonalds"
-        }
-    })
+
+    try {
+        const results = await db.query(
+            "INSERT INTO restaurants (name, location, price_range) vaules ($1, $2, $3) returning", [req.body.name,req.body.location,req.body.price_range]
+        )
+        console.log(results)
+        res.status(201).json({
+            status: "sucess",
+            data:{
+                restaurant: results.rows[0]
+            }
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
     
 })
 
